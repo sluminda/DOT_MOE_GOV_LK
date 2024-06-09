97c8c55f2b1ceb410a2218b4e118ef1184ab1593
Luminda Saranga <sluminda@gmail.com>
2024-06-10 01:01:52 +0530

    Census Update

--- JS/home.js
+++ JS/home.js
@@ -30,3 +30,11 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+// Examples
+
+function greetings() {
+  console.log("Hello Lumi");
+}
+
+greetings();
