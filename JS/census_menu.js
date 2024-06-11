fc38fe8f8f4041044f54bd5f5f477c81643dbdbd
Luminda Saranga <sluminda@gmail.com>
2024-06-11 23:20:31 +0530

    Census Menu Finalized

--- JS/census_menu.js
+++ JS/census_menu.js
@@ -1,13 +1,20 @@
-document.getElementById("year2023").addEventListener("click", function () {
-  document
-    .querySelector(".census_btns_container.CC")
-    .classList.remove("hidden");
-  document.querySelector(".census_btns_container.DD").classList.add("hidden");
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const year2023 = document.getElementById("year2023");
+  const year2022 = document.getElementById("year2022");
+  const census2023 = document.querySelector(".census_btns_container.CC");
+  const census2022 = document.querySelector(".census_btns_container.DD");
 
-document.getElementById("year2022").addEventListener("click", function () {
-  document.querySelector(".census_btns_container.CC").classList.add("hidden");
-  document
-    .querySelector(".census_btns_container.DD")
-    .classList.remove("hidden");
+  year2023.addEventListener("click", function () {
+    year2023.classList.add("selected");
+    year2022.classList.remove("selected");
+    census2023.classList.remove("hidden");
+    census2022.classList.add("hidden");
+  });
+
+  year2022.addEventListener("click", function () {
+    year2022.classList.add("selected");
+    year2023.classList.remove("selected");
+    census2022.classList.remove("hidden");
+    census2023.classList.add("hidden");
+  });
 });
