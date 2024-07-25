d73cf94f4dca3d09a9fda23936a28cab4aa7395b
Luminda Saranga <sluminda@gmail.com>
2024-07-25 19:20:02 +0530

    Errors Debugged and Fixed 2

--- PHP/db_config.php
+++ PHP/db_config.php
@@ -4,12 +4,11 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "dot_moe_gov_lk";
-$port = 3308;
+$port = 3306;
 
 try {
 
     $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
-
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
